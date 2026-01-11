abe99992f2570cba11ae2e2560b2c4cffd78bdb1
kittinanth222 <kittinanth222@gmail.com>
2026-01-11 17:50:13 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'dart:html';
+
+void main() {
+  // เข้าถึง element
+  HeadingElement title = querySelector('#title') as HeadingElement;
+  ButtonElement button = querySelector('#btn') as ButtonElement;
+
+  // เมื่อกดปุ่ม
+  button.onClick.listen((event) {
+    title.text = 'คุณกดปุ่มแล้ว!';
+  });
+}
